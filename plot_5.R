fac2d2e8745f1a217c1264b3babc320da358f79d
BarrieFitzgerald <barriefitzgerald@gmail.com>
2014-12-15 12:01:53 -0500

    uploads

---
+++ plot_5.R
@@ -0,0 +1,23 @@
+## Reading the data file
+SCC <- readRDS("data/Source_Classification_Code.rds")
+NEI <- readRDS("data/summarySCC_PM25.rds")
+
+## Question 5: 
+## How have emissions from motor vehicle sources 
+## changed from 1999-2008 in Baltimore City?
+
+## Subsetting data
+MD.onroad <- subset(NEI, fips == 24510 & type == 'ON-ROAD')
+
+# Aggregates
+MD.df <- aggregate(MD.onroad[, 'Emissions'], by = list(MD.onroad$year), sum)
+colnames(MD.df) <- c('year', 'Emissions')
+
+## Plotting and saving data graph
+png('plot_5.png')
+ggplot(data = MD.df, aes(x = year, y = Emissions)) + 
+        geom_bar(aes(fill = year),stat = "identity") + guides(fill = F) + 
+        ggtitle('Total Emissions of Motor Vehicle Sources in Baltimore City, Maryland') + 
+        ylab(expression('PM'[2.5])) + xlab('Year') + theme(legend.position = 'none') + 
+        geom_text(aes(label = round(Emissions, 0), size = 1, hjust = 0.5, vjust = 2))
+dev.off()
